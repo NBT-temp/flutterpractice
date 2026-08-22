73ad5fedf778a4813bd9401c71e8c15c0824af40
kuccoding <kuc1029@gmail.com>
2026-08-22 17:06:55 +0900

    계산기B 버튼 모양

--- lib/calculator_b.dart
+++ lib/calculator_b.dart
@@ -7,7 +7,178 @@ class CalculatorBPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('계산기B')),
-      body: const SizedBox.shrink(),
+      body: Column(
+        children: [
+          Container(
+            height: 150,
+            alignment: Alignment.centerRight,
+            child: const Text(
+              '0',
+              style: TextStyle(
+                fontSize: 40,
+              ),
+            ),
+          ),
+
+          Row(
+            children: [
+              Expanded(
+                child: SizedBox(
+                  height:80,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('7'),
+                  ),
+                )
+              ),
+              Expanded(
+                child: SizedBox(
+                  height:80,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('8'),
+                  ),
+                )
+              ),
+              Expanded(
+                child: SizedBox(
+                  height:80,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('9'),
+                  ),
+                )
+              ),
+              Expanded(
+                child: SizedBox(
+                  height:80,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('x'),
+                  ),
+                )
+              ),
+            ],
+          ),
+
+          Row(
+            children: [
+              Expanded(
+                child: SizedBox(
+                  height:80,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('4'),
+                  ),
+                )
+              ),
+              Expanded(
+                child: SizedBox(
+                  height:80,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('5'),
+                  ),
+                )
+              ),
+              Expanded(
+                child: SizedBox(
+                  height:80,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('6'),
+                  ),
+                )
+              ),
+              Expanded(
+                child: SizedBox(
+                  height:80,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('-'),
+                  ),
+                )
+              ),
+            ],
+          ),
+
+          Row(
+            children: [
+              Expanded(
+                child: SizedBox(
+                  height:80,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('1'),
+                  ),
+                )
+              ),
+              Expanded(
+                child: SizedBox(
+                  height:80,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('2'),
+                  ),
+                )
+              ),
+              Expanded(
+                child: SizedBox(
+                  height:80,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('3'),
+                  ),
+                )
+              ),
+              Expanded(
+                child: SizedBox(
+                  height:80,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('+'),
+                  ),
+                )
+              ),
+            ],
+          ),
+
+          Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: SizedBox(
+                  height:80,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('0'),
+                  ),
+                )
+              ),
+              Expanded(
+                flex: 1,
+                child: SizedBox(
+                  height:80,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('.'),
+                  ),
+                )
+              ),
+              Expanded(
+                flex: 1,
+                child: SizedBox(
+                  height:80,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('='),
+                  ),
+                )
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
